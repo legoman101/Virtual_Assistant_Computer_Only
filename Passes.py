be445e2d6d3ab87170843f95da894af7872f98a1
legoman101 <yodalovesmindstorms@gmail.com>
2020-06-11 17:11:52 +0100

    updates to secure passes

--- Passes.py
+++
@@ -1,7 +0,0 @@
-#Passes.py
-#Passwords:
-wolframppid: "******-**********"
-Your_Username = "youremail@gmail.com"
-Codingemail = "yourcodingemail@gmail.com"
-Normalemail = "yournormalemail@gmail.com"
-Your_Password = "****************"
